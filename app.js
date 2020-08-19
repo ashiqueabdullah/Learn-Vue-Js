71dabcb8774318ed992c364170ed8f522aa870c3
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-19 18:58:22 +0600

    Argument and modifier

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 new Vue({
     el:"#app",
     data:{
-        x:0,
-        y:0,
         name:'Ashique Abdullah'
     },
     methods:{
-        update(event){
-            this.x=event.clientX,
-            this.y=event.clientY
-        },
-        clk(){
-            this.name="Sidra Ashraf"
+        clk(name){
+            this.name=name
         }
     }
     
