9972a03538f2a35307d4b69e525f78b08fecf8ee
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-19 21:07:18 +0600

    more instance

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-new Vue({
-    el:"#app",
+const app1 =new Vue({
+    el:'#app1',
     data:{
-       a:0
+        user:{
+            name:'Ashique Abdullah',
+            age:24,
+            occupation:'Software Engineer'
+        }
     },
     methods:{
-        
+        update(){
+            app2.user.name="Sidra Ashraf" 
+        }
     }
-    
-    
-    
+});
+
+
+const app2=new Vue({
+    el:'#app2',
+    data:{
+        user:{
+            name:'Ashique Abdullah',
+            age:24,
+            occupation:'Software Engineer'
+        }
+    }
 });
