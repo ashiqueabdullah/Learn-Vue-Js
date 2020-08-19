d56a0dbba0ebb5b5ae0ad65e4bc67b5331a4f19e
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-19 15:41:36 +0600

    for loop

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 new Vue({
     el:"#app",
     data:{
-        as:{
-            name:"Ashique Abdullah",
+        ar:[1,2,3,4,5],
+        user:{
+            name:'Ashiquu Abdullah',
             Age:24,
-            Occupation:"Software Engineer"
-        },
-        si:{
-            name:"Sidra Ashraf",
-            Age:23,
-            Occupation:"Software Engineer" 
-        } 
-    },
-    methods:{
-        husband(){
-            return this.name,this.Age,this.Occupation
-        },
-        wife(){
-            return this.name,this.Age,this.Occupation
-        },
-        condition(pr){
-            return pr==="Husband" ? true : false;
+            Occupation:'Software Engineer'
         }
     }
+    
 });
