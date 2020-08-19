f2fb353105f7f6acfed7dae93f7bf987d0cf612a
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-08-19 20:52:31 +0600

    @

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 new Vue({
     el:"#app",
     data:{
-       a:0,
-       b:0,
-       salary:10
+       a:0
     },
-    computed:{
-        as(){
-            console.log("A +");
-            return this.a + this.salary;
-        },
-        bs(){
-            console.log("B +");
-            return this.b + this.salary;
-        }
-
+    methods:{
+        
     }
+    
     
     
 });
